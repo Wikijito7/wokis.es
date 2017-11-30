e1b122055e89214de3ab054f83ed7c0d0dc96fe2
Wikijito7 <wikyfg@gmail.com>
2017-11-30 21:54:02 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Título de página</title>
+    <link rel="stylesheet" type="text/css" href="index.css" />
+</head>
+<body>
+    <div id="first">
+        <p>Párrafo</p>
+    </div>
+    <div id="second">
+        <span>Capa</span>
+    </div>
+</body>
+</html>
