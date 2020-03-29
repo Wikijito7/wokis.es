a8365c918a5fd583f8e5a5058c1a35964efe96e1
Wikijito7 <wikyfg@gmail.com>
2020-03-29 13:58:08 +0200

    Some hot-fixing for the web.
    Ty Pauli, once again.

--- new/js/wokis.js
+++ new/js/wokis.js
@@ -2,7 +2,7 @@ var lista = false;
 var navlist = document.getElementsByClassName("navlist");
 
 function toggleList() {
-    if(window.outerWidth > 740) return;
+    if(window.outerWidth > 760) return;
     Array.prototype.forEach.call(navlist, function(navlist) {
         if (lista){
             navlist.classList.replace("visible", "invisible");
