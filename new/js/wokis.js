f4b11d9ea7baba2e3e3b8085b97b857921180c57
Wikijito7 <wikyfg@gmail.com>
2020-03-29 13:32:17 +0200

    responsive! kinda part 4.
    That button when looking from mobile things now works!
    More things done! Now it may look better..
    More more things done! Js for that btn as well.
    More more more things done inside! Css kinda looks a little more shit, but it's ok for me..
    More more more more things done! Not really.. just added 3 more thingies on projects to see how it looks.

---
+++ new/js/wokis.js
@@ -0,0 +1,15 @@
+var lista = false;
+var navlist = document.getElementsByClassName("navlist");
+
+function toggleList() {
+    if(window.outerWidth > 740) return;
+    Array.prototype.forEach.call(navlist, function(navlist) {
+        if (lista){
+            navlist.classList.replace("visible", "invisible");
+            return;
+        }
+        navlist.classList.replace("invisible", "visible");
+    });
+
+    lista = !lista;
+}
